34a73590e864ec37e125ba139899dc997f66f14b
Developer <dev@mtg-signature-tracker.local>
2026-08-05 19:31:59 +0000

    fix(dialog): gallery 78dvh, version switch 83dvh

--- src/components/artist-gallery-dialog.tsx
+++ src/components/artist-gallery-dialog.tsx
@@ -29,7 +29,7 @@ export default function ArtistGalleryDialog({
     <Dialog
       open
       onOpenChange={onClose}
-      className="max-w-3xl flex flex-col h-[73dvh] md:h-[80vh] !max-h-[73dvh] md:!max-h-[80vh] overflow-hidden pr-0"
+      className="max-w-3xl flex flex-col h-[78dvh] md:h-[80vh] !max-h-[78dvh] md:!max-h-[80vh] overflow-hidden pr-0"
     >
       <DialogHeader className="shrink-0 px-6 pt-4 pb-2">
         <DialogTitle>{artist} 的卡牌</DialogTitle>
